14713ee50cedf5833e32181116160153c45d2b5f
Edwin Vergara <heydot.at@gmail.com>
2016-03-05 10:56:27 -0600

    Make background change color

--- Background Colors/ViewController.swift
+++ Background Colors/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSlider: UISlider!
     
     @IBAction func changeColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
         
+        self.view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
     
     
