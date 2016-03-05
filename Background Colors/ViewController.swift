017ec84e7f328328a0002b160f0a769b11942e45
Edwin Vergara <heydot.at@gmail.com>
2016-03-05 10:53:21 -0600

    Make connections

--- Background Colors/ViewController.swift
+++ Background Colors/ViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBAction func changeColor() {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
